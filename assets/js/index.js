0546c926ea02880f180808f11927bf1670f8b982
AVATAR197 <39923116+AVATAR197@users.noreply.github.com>
2020-01-28 14:27:49 +0100

    website v2 upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,31 @@
-document.querySelector('.hamburger-button').addEventListener('click', () => {
-    document.querySelector('.navbar-content').classList.toggle('show');
-    document.querySelectorAll('.hm-div').forEach((e) => {
-        e.classList.toggle('active');
-    });
-    document.querySelector('.hamburger-button').classList.toggle('posFixed');
+const show_my_work_button = document.querySelector('.show-my-work-button');
+const my_projects = document.querySelector('.my-projects');
+
+show_my_work_button.addEventListener('click', () => {
+	const top = my_projects.getBoundingClientRect().top;
+	window.scrollTo(0, top);
 });
 
+const anim = document.querySelectorAll('.scroll-anim');
 
+const createObserver = () => {
+	let observer;
 
+	let options = {
+		threshold: 0.8
+	};
+
+	observer = new IntersectionObserver((e) => {
+		e.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.style.animation = `anim 1s forwards ease`;
+			}
+		});
+	}, options);
+
+	anim.forEach((img) => {
+		observer.observe(img);
+	});
+};
+
+window.addEventListener('load', createObserver);
