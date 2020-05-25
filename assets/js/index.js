44e23e2a6b3457fba5a7ce9d8016380cf24607c8
AVATAR197 <39923116+AVATAR197@users.noreply.github.com>
2020-05-26 00:54:26 +0200

    update 05/26/2020

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,31 @@
-const show_my_work_button = document.querySelector('.show-my-work-button');
-const my_projects = document.querySelector('.my-projects');
+const show_my_work_button = document.querySelector(".show-my-work-button");
+const my_projects = document.querySelector(".my-projects");
 
-show_my_work_button.addEventListener('click', () => {
-	const top = my_projects.getBoundingClientRect().top;
-	window.scrollTo(0, top);
+show_my_work_button.addEventListener("click", () => {
+    const top = my_projects.getBoundingClientRect().top;
+    window.scrollTo(0, top);
 });
 
-const anim = document.querySelectorAll('.scroll-anim');
+const anim = document.querySelectorAll(".scroll-anim");
 
 const createObserver = () => {
-	let observer;
+    let observer;
 
-	let options = {
-		threshold: 0.8
-	};
+    let options = {
+        threshold: 0.55,
+    };
 
-	observer = new IntersectionObserver((e) => {
-		e.forEach((entry) => {
-			if (entry.isIntersecting) {
-				entry.target.style.animation = `anim 1s forwards ease`;
-			}
-		});
-	}, options);
+    observer = new IntersectionObserver((e) => {
+        e.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.style.animation = `anim 1s forwards ease`;
+            }
+        });
+    }, options);
 
-	anim.forEach((img) => {
-		observer.observe(img);
-	});
+    anim.forEach((img) => {
+        observer.observe(img);
+    });
 };
 
-window.addEventListener('load', createObserver);
+window.addEventListener("load", createObserver);
